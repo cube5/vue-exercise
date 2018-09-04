0fff9e9deedbd3c7ac7aee8b685926feb0a773f8
Daniel Sanchez <jdaniel.saed@gmail.com>
2018-09-04 09:55:55 -0500

    load user data when component created

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
   <div id="app">
-    Hello, world!
+    <app-header></app-header>
+    <app-posts></app-posts>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader';
+import AppPosts from './views/AppPosts';
+
 export default {
   name: 'App',
+  components: { AppHeader, AppPosts },
+  created() {
+    const userId = Math.floor(Math.random() * 10) + 1; // random between 1 - 10
+    this.$store.dispatch('user/fetchUserProfile', userId);
+  },
 };
 </script>
+
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
